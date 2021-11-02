2169bf98d76160121d85fc88b3bc9fc1ab14294e
mntone <sd8@live.jp>
2021-11-02 20:12:20 +0900

    String.toQuery() publicized.

--- Sources/HttpParameterKit/String+Extensions.swift
+++ Sources/HttpParameterKit/String+Extensions.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension String {
+public extension String {
 	func toQuery() -> String {
 		replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
 			.addingPercentEncoding(withAllowedCharacters: .queryAllowed)!
